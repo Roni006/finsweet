dbb58185cd3de1db41954e662b6d41b6d1569431
Anik Islam <anik54908@gmail.com>
2026-01-03 22:23:39 +0600

    pricing section added

--- src/Pages/Pricing/Pricing.jsx
+++ src/Pages/Pricing/Pricing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "../../components/Common/Container";
-
+import ColorPointer from "../../assets/ColorPointer.svg";
+import BWPointer from "../../assets/BWPointer.svg";
 const Pricing = () => {
   return (
     <>
@@ -9,9 +10,212 @@ const Pricing = () => {
           <div>
             <div className="w-[562px] text-center mx-auto mb-15">
               <h1 className="gblh">Our Pricings Plan</h1>
-              <p className="gblf">When you’re ready to go beyond prototyping in Figma, Webflow is ready to help you bring your designs to life — without coding them.</p>
+              <p className="gblf">
+                When you’re ready to go beyond prototyping in Figma, Webflow is
+                ready to help you bring your designs to life — without coding
+                them.
+              </p>
             </div>
-            <div></div>
+            <div className="mb-[120px] flex gap-7 items-center">
+              <div className="bg-[#f4f6fc] w-[406px] pt-11 pb-[62px] ps-12 pe-[76px] rounded-md ">
+                <h2 className="flex items-center gap-[5px] text-[38px] font-semibold font-poppins text-[#282938] leading-14 ">
+                  $299{" "}
+                  <span className="font-normal leading-7 text-[#2405f2] text-[16px]  ">
+                    Per Design
+                  </span>
+                </h2>
+                <h3 className="font-medium font-poppins text-[24px] leading-9 text-[#282938] mt-5">
+                  Landing Page{" "}
+                </h3>
+                <p className="font-normal font-poppins leading-7 text-[#282938] w-[281px] pt-2 ">
+                  When you’re ready to go beyond prototyping in Figma,{" "}
+                </p>
+                <div className="mt-[78px] flex flex-col gap-y-5">
+                  <div className=" flex items-center gap-3 ">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">All limited links</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">All limited links</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">Own analytics platform</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">Chat support</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={BWPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">Optimize hashtags</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={BWPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">Unlimited users</p>
+                  </div>
+                </div>
+                <div className="  text-center flex items-center justify-center mx-auto ] ">
+                  <a
+                    href="#"
+                    className="py-4 px-[77px] bg-[#282938] text-white text-[18px] leading-8 font-semibold font-mono rounded-full cursor-pointer mt-20  "
+                  >
+                    Get Started
+                  </a>
+                </div>
+              </div>
+
+              <div className="bg-[#1C1E53] text-white w-[406px] pt-11 pb-[62px] ps-12 pe-[76px] rounded-md ">
+                <h2 className="flex items-center gap-[5px] text-[38px] font-semibold font-poppins text-[#ffffff] leading-14 ">
+                  $399
+                  <span className="font-normal leading-7 text-[#FCD980] text-[16px]  ">
+                    Multi Design
+                  </span>
+                </h2>
+                <h3 className="font-medium font-poppins text-[24px] leading-9 text-[#ffffff] mt-5">
+                  Website Page
+                </h3>
+                <p className="font-normal font-poppins leading-7 text-[#ffffff] w-[281px] pt-2 ">
+                  When you’re ready to go beyond prototyping in Figma, Webflow’s
+                  ready to help.
+                </p>
+                <div className="mt-[78px] flex flex-col gap-y-5">
+                  <div className=" flex items-center gap-3 ">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf !text-white">All limited links</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf !text-white">All limited links</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf !text-white">Own analytics platform</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf !text-white">Chat support</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf !text-white">Optimize hashtags</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf !text-white">Unlimited users</p>
+                  </div>
+                </div>
+                <div className="  text-center flex items-center justify-center mx-auto ] ">
+                  <a
+                    href="#"
+                    className="py-4 px-[77px] bg-[#FCD980] text-[#282938] text-[18px] leading-8 font-semibold font-mono rounded-full cursor-pointer mt-20  "
+                  >
+                    Get Started
+                  </a>
+                </div>
+              </div>
+
+              <div className="bg-[#f4f6fc] w-[406px] pt-11 pb-[62px] ps-12 pe-[76px] rounded-md ">
+                <h2 className="flex items-center gap-2.5 text-[38px] font-semibold font-poppins text-[#282938] leading-14 ">
+                  $499 +
+                  <span className="font-normal leading-7 text-[#2405f2] text-[16px]  ">
+                    Per Design
+                  </span>
+                </h2>
+                <h3 className="font-medium font-poppins text-[24px] leading-9 text-[#282938] mt-5">
+                  Complex Project
+                </h3>
+                <p className="font-normal font-poppins leading-7 text-[#282938] w-[281px] pt-2 ">
+                  When you’re ready to go beyond prototyping in Figma,
+                </p>
+                <div className="mt-[78px] flex flex-col gap-y-5">
+                  <div className=" flex items-center gap-3 ">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">All limited links</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">All limited links</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">Own analytics platform</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">Chat support</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">Optimize hashtags</p>
+                  </div>
+
+                  <div className=" flex items-center gap-3">
+                    <span>
+                      <img src={ColorPointer} alt=" price icon" />
+                    </span>
+                    <p className="gblf">Unlimited users</p>
+                  </div>
+                </div>
+                <div className="text-center flex items-center justify-center mx-auto ">
+                  <a
+                    href="#"
+                    className="py-4 px-[77px] bg-[#282938] text-white text-[18px] leading-8 font-semibold font-mono rounded-full cursor-pointer mt-20  "
+                  >
+                    Get Started
+                  </a>
+                </div>
+              </div>
+            </div>
           </div>
         </Container>
       </section>
--- src/components/Common/Navbar/Navbar.jsx
+++ src/components/Common/Navbar/Navbar.jsx
@@ -1,38 +1,57 @@
-// ! image import 
-import { Link } from 'react-router'
-import logo from '../../../assets/images/logo.png'
-// ! image import 
-import Container from '../Container'
+// ! image import
+import { Link } from "react-router";
+import logo from "../../../assets/images/logo.png";
+// ! image import
+import Container from "../Container";
 const Navbar = () => {
-    return (
-        <><section className='bg-[#1C1E53]'>
-            <Container>
-                <div className="mainContent flex items-center justify-between py-5">
-                    <div className="content1">
-                        <a href='#'>
-                            <img src={logo} alt="logo" />
-                        </a>
-                    </div>
-                    <div className="content2 flex items-center gap-12">
-                        <div className='menus'>
-                            <ul className='flex items-center gap-8'>
-                                <li><a href="#" >Home</a></li>
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="#">Features</a></li>
-                                <li><a href="#">Pricing</a></li>
-                                <li><a href="#">FAQ</a></li>
-                                <li><a href="#">Blog</a></li>
-                            </ul>
-                        </div>
-                        <div className='btn'>
-                            <button className='py-4 px-12 border-2 border-[rgba(244,246,252,0.2)] rounded-[40px] cursor-pointer font-medium '>Contact Us</button>
-                        </div>
-                    </div>
-                </div>
-            </Container>
-        </section>
-        </> 
-    )
-}
+  return (
+    <>
+      <section className="bg-[#1C1E53]">
+        <Container>
+          <div className="mainContent flex items-center justify-between py-10">
+            <div className="content1">
+              <Link to="/">
+                <img src={logo} alt="logo" />
+              </Link>
+            </div>
+            <div className="content2 flex items-center gap-12">
+              <div className="menus">
+                <ul className="flex items-center gap-8">
+                  <li>
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li>
+                    <Link to="/">About Us</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Features</Link>
+                  </li>
+                  <li>
+                    <Link to="/pricing">Pricing</Link>
+                  </li>
+                  <li>
+                    <Link to="/">FAQ</Link>
+                  </li>
+                  <li>
+                    <Link to="/">Blog</Link>
+                  </li>
+                </ul>
+              </div>
+              <div className="btn">
+                <Link
+                  to="/contact"
+                  className="button py-4 px-12 border-2 border-[rgba(244,246,252,0.2)] rounded-[40px] cursor-pointer font-medium "
+                >
+                  Contact Us
+                </Link>
+                {/* <button className='py-4 px-12 border-2 border-[rgba(244,246,252,0.2)] rounded-[40px] cursor-pointer font-medium '>Contact Us</button> */}
+              </div>
+            </div>
+          </div>
+        </Container>
+      </section>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
