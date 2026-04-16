717a18291947b4508f3132a3bdbee4b6fdd1ff62
Anik Islam <anik54908@gmail.com>
2026-04-16 12:30:10 +0600

    aling all the components of read blog

---
+++ src/components/ReadBlog/ReadBlog.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BlogHeading from "../../Pages/BlogHeading/BlogHeading";
+import BlogBody from "../../Pages/BlogBody/BlogBody";
+
+const ReadBlog = () => {
+  return (
+    <>
+      <BlogHeading />
+      <BlogBody />
+    </>
+  );
+};
+
+export default ReadBlog;
