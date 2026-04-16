9fc1cc1e16188c12ca64568fe268364971b606b1
Anik Islam <anik54908@gmail.com>
2026-04-16 11:48:00 +0600

    initialize Features meuns

---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Container from '../Common/Container'
+
+const Features = () => {
+  return (
+    <>
+    <section>
+        <Container>
+            
+        </Container>
+    </section>
+    </>
+  )
+}
+
+export default Features
